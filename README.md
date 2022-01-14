cc7f3e7dd2b0a32721092655283305e9629f5929
Carolina Todt <carolina.todt@rosariosoftware.com>
2022-01-14 17:23:55 -0300

    fix(front/back):fixes in the UI

--- README.md
+++ README.md
@@ -4,21 +4,21 @@
 Se debe crear una aplicación de votación realtime.
 
 ## API
-* Debe exponer un servidor de websocket al que se pueda subscribir.
-* Debe emitir eventos cuando haya votos nuevos.
+* Debe exponer un servidor de websocket al que se pueda subscribir.[x]
+* Debe emitir eventos cuando haya votos nuevos.[x]
 
 ## Cliente
-* Debe haber al menos dos productos sobre los cuales se pueda votar.
-* Se debe mostrar un indicador del porcentaje de votos de cada producto.
-* Se debe poder ver quienes fueron los votantes y sus respectivas valoraciónes.
-* Cada usuario puede votar una vez, si vota más de una, el voto se transfiere.
+* Debe haber al menos dos productos sobre los cuales se pueda votar. [x]
+* Se debe mostrar un indicador del porcentaje de votos de cada producto.[x]
+* Se debe poder ver quienes fueron los votantes y sus respectivas valoraciónes.[x]
+* Cada usuario puede votar una vez, si vota más de una, el voto se transfiere.[x]
 
 ## Definiciones técnicas
-* El código de la aplicación debe estar subida a un repositorio de público acceso.
+* El código de la aplicación debe estar subida a un repositorio de público acceso.[x]
 
 ## Puntos extra
 * El usuario puede seleccionar los productos desde el cliente viendo un modal con productos de Mercado Libre.
-* La votación se puede pausar, reanudar y reiniciar desde el cliente.
+* La votación se puede pausar, reanudar y reiniciar desde el cliente.[x]
 
 ![Spec](./spec.png)
 
--- packages/client/src/app/screens/Home/Home.tsx
+++ packages/client/src/app/screens/Home/Home.tsx
@@ -4,7 +4,7 @@ import SocketIO from "socket.io-client";
 import logo from "~/assets/logo.svg";
 
 import styles from "./Home.module.scss";
-import {Product, Vote, DataServer} from "../../../../../server/src/types";
+import { Product, Vote, DataServer } from "../../../../../server/src/types";
 
 const socket = SocketIO.io("http://localhost:5000");
 
@@ -12,7 +12,6 @@ const Home: React.FC = () => {
   const [products, setProducts] = React.useState<Product[]>([]);
   const [votes, setVotes] = React.useState<Vote[]>([]);
   const [state, setState] = React.useState<"connected" | "disconnected">("connected");
-  const [renders, setRender] = React.useState<number>(0);
 
   const addVote = (key: string) => {
     if (state === "disconnected") return;
@@ -32,28 +31,19 @@ const Home: React.FC = () => {
 
     setVotes(newVotes);
 
-    console.log("subida voto");
-    setRender((renders) => renders + 1);
-    socket.emit("addvote", {product: key, user: userNew, review: reviewNew} as Vote);
+    socket.emit("addvote", { product: key, user: userNew, review: reviewNew } as Vote);
   };
 
   socket.on("stateServer", (stateServer: "connected" | "disconnected") => {
     setState(stateServer);
   });
 
-  let num = 1;
 
   socket.on("state", (data: DataServer) => {
     setProducts(data.products);
     setVotes(data.votes);
   });
 
-
-  React.useEffect(() => {
-    setState("connected");
-    setRender((render) => render + 1);
-    console.log(`use Effect ${num++}`);
-  }, []);
 
   const changeConnection = (status: string) => {
     if (status === "disconnect") {
@@ -80,35 +70,37 @@ const Home: React.FC = () => {
           <img alt="RealTrends" src={logo} width={180} />
         </h1>
         <h3>Lets get this party started</h3>
+        <h2>Total Vote(s) {votes.length}</h2>
       </header>
-     
+   
       <section className={styles.products}>
         {products.length ? (
-        products.map(product => {
-          const productVotes = votes.filter((vote) => vote.product === product.key)
-          const percentage = (productVotes.length * 100) / votes.length;
-          return (
-            <article key={product.key}>
-               <div className={styles.product} onClick={() => addVote(product.key)}>
-              <div>
-                <h3>   {product.title}</h3>
-                <h5>{product.description}</h5>
-                <img src={product.image} alt={product.title} />
-                <p>{productVotes.length} voto(s)</p>
-                <ul>
-                  {productVotes.map(vote =>
-                    <li key={vote.user}>
-                      <b>{vote.user}</b>
-                      <span> {vote.review || '-'}</span>
-                    </li>
-                  )}
-                </ul>
-              </div>
-              <aside style={{ height: `${percentage}%`, backgroundColor: `hsl(${percentage},100%,50%)` }} />
-              </div>
-            </article>
-          )
-        })
+          products.map(product => {
+            const productVotes = votes.filter((vote) => vote.product === product.key)
+            const percentage = (productVotes.length * 100) / votes.length;
+            return (
+        
+              <article key={product.key}>
+                <div className={styles.product} onClick={() => addVote(product.key)}>
+                  <div>
+                    <h3>   {product.title}</h3>
+                    <h5>{product.description}</h5>
+                    <img src={product.image} alt={product.title} />
+                    <p>{productVotes.length} vote(s)</p>
+                    <ul>
+                      {productVotes.map(vote =>
+                        <li key={vote.user}>
+                          <b>{vote.user}</b>
+                          <span> {vote.review || '-'}</span>
+                        </li>
+                      )}
+                    </ul>
+                  </div>
+                  <aside style={{ height: `${percentage}%`, backgroundColor: `hsl(${percentage},100%,50%)` }} />
+                </div>
+              </article>
+            )
+          })
 
         ) : (
           <h3>Loading...</h3>
